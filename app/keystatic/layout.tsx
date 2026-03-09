c5cb53da0132271874e09b6419c9a8d7adcde51d
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2026-03-09 19:59:41 +0530

    fix: wrap event types write in dev mode

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -7,17 +7,22 @@ import { fileURLToPath } from "node:url"
 export default async function Layout() {
   const eventTypes = await fetchEventTypes()
 
-  const filename = fileURLToPath(import.meta.url)
+  if (process.env.NODE_ENV === "development") {
+    const filename = fileURLToPath(import.meta.url)
 
-  const destinationDirname = path.join(path.dirname(filename), ".../../assets")
-  const destinationPath = path.join(destinationDirname, "event-types.json")
+    const destinationDirname = path.join(
+      path.dirname(filename),
+      ".../../assets",
+    )
+    const destinationPath = path.join(destinationDirname, "event-types.json")
 
-  await fs.promises.mkdir(destinationDirname, { recursive: true })
-  await fs.promises.writeFile(
-    destinationPath,
-    JSON.stringify(eventTypes, null, 2),
-    "utf-8",
-  )
+    await fs.promises.mkdir(destinationDirname, { recursive: true })
+    await fs.promises.writeFile(
+      destinationPath,
+      JSON.stringify(eventTypes, null, 2),
+      "utf-8",
+    )
+  }
 
   return <KeystaticApp />
 }
